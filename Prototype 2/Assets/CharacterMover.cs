14cc2e8d3c6cfaf92ecfe0ed12924d94bea88d74
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-09-28 12:17:19 -0600

    Adding Animals

---
+++ Prototype 2/Assets/CharacterMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public float horizontalInput;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
