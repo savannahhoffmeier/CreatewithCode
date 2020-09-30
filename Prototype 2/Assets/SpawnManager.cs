55df902e60dfe07983ebc3d9906b002315a94675
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-09-30 17:06:08 -0600

    Game Fix

---
+++ Prototype 2/Assets/SpawnManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    private float spawnRangex = 20;
+    private float spawnpoz = 20;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos=new Vector3(Random.Range(-spawnRangex,spawnRangex),0,spawnpoz);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
+        }
+    }
+}
+
