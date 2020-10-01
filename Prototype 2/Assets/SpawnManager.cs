a95ca70b4bdbffd826d5f8826458738be73f4cca
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-10-01 15:36:03 -0600

    Tried to Fix a Bug
    It didn't work

--- Prototype 2/Assets/SpawnManager.cs
+++ Prototype 2/Assets/SpawnManager.cs
@@ -1,23 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-
     private float spawnRangex = 20;
     private float spawnpoz = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos=new Vector3(Random.Range(-spawnRangex,spawnRangex),0,spawnpoz);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
 
+        void SpawnRandomAnimal()
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangex, spawnRangex), 0, spawnpoz);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
-
