22a7b4678e0139ea26027a658d3313e0c2cfb9cc
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-10-04 10:33:13 -0600

    Finishing Game

--- Prototype 2/Assets/DestroyoutofBounds.cs
+++ Prototype 2/Assets/DestroyoutofBounds.cs
@@ -22,6 +22,7 @@ public class DestroyoutofBounds : MonoBehaviour
         }
         else if (transform.position.z<lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
