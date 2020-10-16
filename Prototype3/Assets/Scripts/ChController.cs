c7799084fc92746733c97c5a63dca08767391d4b
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-10-16 11:05:33 -0600

    Making Jump

--- Prototype3/Assets/Scripts/ChController.cs
+++ Prototype3/Assets/Scripts/ChController.cs
@@ -1,18 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ChController : MonoBehaviour
 {
-    // Start is called before the first frame update
+private Rigidbody playerRb;
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
     }
 }
