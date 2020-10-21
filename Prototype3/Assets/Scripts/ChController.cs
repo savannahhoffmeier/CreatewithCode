9538f7e79405a689ba6f275e2eac68ad07881e07
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-10-21 13:20:02 -0600

    Adding Movements

--- Prototype3/Assets/Scripts/ChController.cs
+++ Prototype3/Assets/Scripts/ChController.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 public class ChController : MonoBehaviour
 {
 private Rigidbody playerRb;
+public float jumpForce=10;
+public float gravityModifier;
+public bool isOnGround = true;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        
+        Physics.gravity *= gravityModifier;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRb.AddForce(Vector3.up*100,ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up*jumpForce,ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
