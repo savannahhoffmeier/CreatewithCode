d5bd3b38747c82455120e10ff41bda202c847d6c
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-10-26 10:53:46 -0600

    FOllowing Cam

---
+++ Prototype 4/Assets/Scripts/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject focalPoint;
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward*speed*Time.deltaTime);
+    }
+}
