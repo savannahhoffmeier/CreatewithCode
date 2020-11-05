09c8d12d92015a407f787b6ba15b3ebcd6d862c9
Savannah Hoffmeier <10858457@my.uvu.edu>
2020-11-05 15:51:10 -0700

    keeping Score

--- Protottype 5/Assets/Course Library/Scripts/Target.cs
+++ Protottype 5/Assets/Course Library/Scripts/Target.cs
@@ -12,6 +12,9 @@ public class Target : MonoBehaviour
     private float maxTorque=10;
     private float xRange=4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,7 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(Random.Range(-maxTorque,maxTorque), Random.Range(-maxTorque,maxTorque),Random.Range(-maxTorque,maxTorque), ForceMode.Impulse);
         
         transform.position=new Vector3(Random.Range(-xRange,xRange),-ySpawnPos,0);
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -46,6 +50,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
